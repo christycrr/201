be2f1fce173b2b4843d89fd8a2dc6768c7660dc9
Christy Carr <ccarr9134@gmail.com>
2022-10-26 00:51:52 +0000

    Lab2

---
+++ JS/app.js
@@ -0,0 +1,46 @@
+console.log ('Hello World');
+let userResponse = prompt('What is your name?');
+console.log('user response to Q1: ' + userResponse);
+alert('Hey ' + userResponse);
+let userResponseTwo = prompt('What is your favorite color?');
+console.log('User entered their favorite color as' + userResponseTwo);
+alert('Wow ' + userResponse + 'your favorite color is ' + userResponseTwo );
+let userResponseThree = prompt('Do you want to hang out with me?');
+if( userResponseThree === 'yes'){
+    alert('We should be friends!' + userResponse );  
+}
+else{
+    alert(`That's too bad ${userResponse}`);
+}
+
+console.log('I am now friends with' + userResponse);
+let userResponseFour = prompt('Did we just become best friends?');
+if( userResponseFour === 'yes'){
+    alert(`Let's hang out!` + userResponse);  
+}
+else{
+    alert(`That's too bad ${userResponse}`);
+}
+alert('I have power tools!' + userResponseFour );
+console.log(userResponseFour + 'is our best friend');
+let userResponseFive = prompt('Could ya learn to love me?');
+if( userResponseFive === 'yes'){
+    alert('Smart' + userResponse );  
+}
+else{
+    alert(`That's too bad ${userResponse}`);
+}
+console.log( userResponse + 'Could learn to love me');
+alert('These are my waters');
+let userResponseSix = prompt('Do you like Old Greg?');
+if( userResponseSix === 'yes'){
+    alert(`He's actually hilarious!` + userResponse );  
+}
+else{
+    alert(`That's too bad ${userResponse}`);
+}
+console.log(' userResponse + likes Old Greg');
+alert(`Don't forget to drink water`);
+
+alert('Thank you for visiting my page' +userResponse);
+// JavaScript will live here
