2cd11112ce382b6662e9f7486e938203bfdea5dc
Christy Carr <ccarr9134@gmail.com>
2022-10-28 00:02:54 +0000

    Lab 3 Completion Question 7

--- JS/app.js
+++ JS/app.js
@@ -1,70 +1,94 @@
 'use strict';
 console.log('Hello World');
 
-
+let correctAnswers = 0;
 let userResponse = prompt('What is your name?');
 console.log('user response to Q1: ' + userResponse);
 alert('Hey ' + userResponse);
 
-function questionOne(){
-let userResponseTwo = prompt('What is your favorite color?');
-console.log('User entered their favorite color as' + userResponseTwo);
-alert('Wow ' + userResponse + 'your favorite color is ' + userResponseTwo);
+function questionOne() {
+    let userResponseTwo = prompt('What is your favorite color?');
+    console.log('User entered their favorite color as' + userResponseTwo);
+    alert('Wow ' + userResponse + 'your favorite color is ' + userResponseTwo);
 }
 questionOne();
 
-function questionThree(){
-let userResponseThree = prompt('Do you want to hang out with me?');
-if (userResponseThree === 'yes') {
-    alert('We should be friends!' + userResponse);
-}
-else {
-    alert(`That's too bad ${userResponse}`);
-}
+function questionThree() {
+    let userResponseThree = prompt('Do you want to hang out with me?');
+    if (userResponseThree === 'yes') {
+        alert('We should be friends!' + userResponse);
+    }
+    else {
+        alert(`That's too bad ${userResponse}`);
+    }
 
 }
 questionThree();
 
 console.log('I am now friends with' + userResponse);
 
-function questionFour(){
-let userResponseFour = prompt('Did we just become best friends?');
-if (userResponseFour === 'yes') {
-    alert(`Let's hang out!` + userResponse);
-}
-else {
-    alert(`That's too bad ${userResponse}`);
-}
-alert('I have power tools!' + userResponseFour);
+function questionFour() {
+    let userResponseFour = prompt('Did we just become best friends?');
+    if (userResponseFour === 'yes') {
+        alert(`Let's hang out!` + userResponse);
+    }
+    else {
+        alert(`That's too bad ${userResponse}`);
+    }
+    alert('I have power tools!' + userResponseFour);
 }
 questionFour();
 
-function questionFive(){
-console.log(userResponseFour + 'is our best friend');
-let userResponseFive = prompt('Could ya learn to love me?');
-if (userResponseFive === 'yes') {
-    alert('Smart' + userResponse);
-}
-else {
-    alert(`That's too bad ${userResponse}`);
-}
+function questionFive() {
+    
+    let userResponseFive = prompt('Could ya learn to love me?');
+    console.log(userResponseFive + 'is our best friend');
+    if (userResponseFive === 'yes') {
+        alert('Smart' + userResponse);
+    }
+    else {
+        alert(`That's too bad ${userResponse}`);
+    }
 }
 questionFive();
 
-function questionSix(){
-console.log(userResponse + 'Could learn to love me');
-alert('These are my waters');
-let userResponseSix = prompt('Do you like Old Greg?');
-if (userResponseSix === 'yes') {
-    alert(`He's actually hilarious!` + userResponse);
-}
-else {
-    alert(`That's too bad ${userResponse}`);
-}
-console.log(' userResponse + likes Old Greg');
-alert(`Don't forget to drink water`);
+function questionSix() {
+    console.log(userResponse + 'Could learn to love me');
+    alert('These are my waters');
+    let userResponseSix = prompt('Do you like Old Greg?');
+    if (userResponseSix === 'yes') {
+        alert(`He's actually hilarious!` + userResponse);
+    }
+    else {
+        alert(`That's too bad ${userResponse}`);
+    }
+    console.log(' userResponse + likes Old Greg');
+    alert(`Don't forget to drink water`);
 }
 questionSix();
+
+let q7answers = ['Barbados', 'Greece', 'Your house', 'My house', 'Switzerland', 'Iceland', 'Hawaii'];
+
+
+function questionSeven() {
+    for (let i = 0; i < 6; i++) {
+
+        let userResponseSeven = prompt('Where would I like to travel first?');
+        for (let j = 0; j < q7answers.length; j++) {
+            if (userResponse === q7answers[j]) {
+                correctAnswers++;
+                
+                alert('That\'s right!');
+                break;
+            }
+        }
+        alert('That\'s incorrect! Try again!');
+    }
+    alert(`You got ${correctAnswers} correct!`)
+    alert('You have exceeded attempts allowed! These are my waters!');
+
+}
+questionSeven();
 
 // LAB 3
 
@@ -99,11 +123,11 @@ let answer 6 = prompt
 let attempts = 0;
 let numbersix;
 
-while (attempts<6) {
-     numbersix = prompt('what is my favorite number?');
+while (attempts < 6) {
+    numbersix = prompt('what is my favorite number?');
     if (numbersix == 7) {
         alert('I can\'t believe you guessed it!');
-        attempts=6;
+        attempts = 6;
     }
     else if (numbersix > 7) {
         alert('Too high!');
@@ -113,11 +137,11 @@ while (attempts<6) {
         alert('Too low!');
         attempts++;
     }
-    else{
+    else {
         alert('That\'s not a number');
         attempts++;
     }
-    
+
 }
 // console.log(nums);
 // let userInput = prompt
